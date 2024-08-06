7f126f353e198faf2c0ac74b54f70de906ddc42a
Shreyan Gupta <61325229+Xenon8778@users.noreply.github.com>
2024-08-06 11:59:35 -0500

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Nkx2-1 KO Sample
+#'
+#' This is only the KO sample.
+#' This example data is borrowed from an
+#' experimental Nkx2-1 Gene knockout scRNA-seq study
+#'
+#' @name KO_count
+#' @docType data
+#' @format A Matrix object.
+#' @details
+#'     details for KO_count
+#' @source <https://doi.org/10.1152/ajplung.00337.2015>
+"KO_count"
+
+#' Nkx2-1 WT Sample
+#'
+#' This is only the WT sample.
+#' This example data is borrowed from an
+#' experimental Nkx2-1 Gene knockout scRNA-seq study
+#'
+#' @name WT_count
+#' @docType data
+#' @format A Matrix object.
+#' @details
+#'     details for WT_count
+#' @source <https://doi.org/10.1152/ajplung.00337.2015>
+"WT_count"
